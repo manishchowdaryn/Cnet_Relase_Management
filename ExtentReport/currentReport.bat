ed185aee11ce6d7785fa73278049c542affa512d
maneesh <maneesh@cnet-global.com>
2019-02-11 10:06:45 -0600

    Test Run in Microsoft Edge Browser

--- ExtentReport/currentReport.bat
+++ ExtentReport/currentReport.bat
@@ -1,1 +1,1 @@
-xcopy /s /y C:\Users\User\eclipse-workspace\Release_Management\ExtentReport\Run_02112019_09_44_45\*.* C:\Users\User\eclipse-workspace\Release_Management\ExtentReport\CurrentReport
+xcopy /s /y C:\Users\User\eclipse-workspace\Release_Management\ExtentReport\Run_02112019_10_03_14\*.* C:\Users\User\eclipse-workspace\Release_Management\ExtentReport\CurrentReport
